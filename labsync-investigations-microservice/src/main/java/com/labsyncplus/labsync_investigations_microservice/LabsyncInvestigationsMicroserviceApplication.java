c18b9769a6850035d28157f0d81dcf1ec1e066e5
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-27 15:00:08 +0530

    Add initial code to investigations microservice

---
+++ labsync-investigations-microservice/src/main/java/com/labsyncplus/labsync_investigations_microservice/LabsyncInvestigationsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.labsyncplus.labsync_investigations_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsyncInvestigationsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsyncInvestigationsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ labsync-investigations-microservice/src/test/java/com/labsyncplus/labsync_investigations_microservice/LabsyncInvestigationsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labsyncplus.labsync_investigations_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsyncInvestigationsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
