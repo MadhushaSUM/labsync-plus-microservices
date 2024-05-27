7c66aeedc671e34adb7e1cb016f9a4f92b87a427
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-27 13:09:33 +0530

    patient microservice initial code

---
+++ labsync-patients-microservice/src/main/java/com/labsyncplus/labsync_patients_microservice/LabsyncPatientsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.labsyncplus.labsync_patients_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsyncPatientsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsyncPatientsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ labsync-patients-microservice/src/test/java/com/labsyncplus/labsync_patients_microservice/LabsyncPatientsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labsyncplus.labsync_patients_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsyncPatientsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
