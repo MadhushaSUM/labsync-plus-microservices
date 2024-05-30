ddbcd0d6306049f757fc1d4b85d7aa120ef2a431
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-05-30 14:30:32 +0530

    Initial investigation data microservice

---
+++ labsync-investigation-data/src/test/java/com/labsync/labsync_investigation_data/LabsyncInvestigationDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labsync.labsync_investigation_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsyncInvestigationDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
